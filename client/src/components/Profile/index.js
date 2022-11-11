2f8e9ac2651951759a7ffe756a16a8f66449de98
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2022-11-10 18:29:40 -0800

    updated profile styling

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/client';
+
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import SkillsList from '../SkillsList';
 import SkillForm from '../SkillForm';
 
-import { QUERY_ME, EDIT_PROFILE } from '../../utils/queries';
+import { QUERY_ME } from '../../utils/queries';
+import { EDIT_PROFILE } from '../../utils/mutations';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import CardHeader from '@mui/material/CardHeader';
 
 import Auth from '../../utils/auth';
 
 import Avatar from '../Avatar';
 
+import EditProfile from '../EditProfile';
 
 const Profile = () => {
   const { profileId } = useParams();
@@ -29,6 +34,10 @@ const Profile = () => {
       variables: { profileId: profileId },
     }
   );
+
+  // // For Edit Profile
+  // const [edits, setEdits] = useState('');
+  // const [editProfile, { error }] = useMutation(EDIT_PROFILE);
 
   // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
   const profile = data?.me || data?.profile || {};
@@ -59,39 +68,71 @@ const Profile = () => {
     );
   }
 
+
+  /////////////// editing data /////////////////
+
+  
+
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+
+  //   try {
+  //     const {data} = await editProfile({
+  //       variables: { education, name, yearsExperience },
+  //     });
+
+  //     setEdits('');
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  /////////////// editing data /////////////////
+
   return (
     <div>
 
       {!isShown ? 
       <Card sx={{ bgcolor: 'text.primary' }}>
-        <CardContent color="text.primary">
-          <Typography variant="h5" component="div" color="primary.main">
-            <Avatar
-
-              name={profile.name} />
-            {`${profile.name}`}
-          </Typography>
+        <CardContent color="primary.main">
+          <CardHeader 
+          avatar={
+          // <Typography variant="h5" component="div" color="primary.main" fontWeight="bold">
+            <Avatar name={profile.name} />
+          }
+          title={<Typography sx={{color: '#052541', fontSize:'25px', fontWeight:'bold', mb:-'1', mt:-'1' }}>{profile.name}</Typography>}
+          subheader= {<Typography sx={{color: '#393D39', fontSize:'15px', }}>{profile.membershipType}</Typography>}
+            />
+          {/* </Typography> */}
+          {/*           
           <Typography variant="body2" color="text.secondary">
               {`${profile.membershipType}`}
-            </Typography>
-          <Typography sx={{ mb: 1.5, fontSize: 12 }} color="text.secondary">
+            </Typography> */}
+          <Typography sx={{ mb: 1.5, fontSize: 15, ml:3, }} color="text.secondary">
             Email: {`${profile.email}`}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography sx={{ mb: 1.5, fontSize: 15, ml:3, }} variant="body2" color="text.secondary">
             Education: {`${profile.education}`}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography sx={{ mb: 1.5, fontSize: 15, ml:3, }} variant="body2" color="text.secondary">
             Year's Experience: {`${profile.yearsExperience}`}
           </Typography>
-          <Typography sx={{ fontSize: 10 }} color="text.secondary" gutterBottom>
-            {`${profile.skills}`}
+          <Typography sx={{ mb: 1.5, fontSize: 15, ml:3, }} color="text.secondary" gutterBottom>
+            Skills: {`${profile.skills} `}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small" onClick={handleEdit}>EDIT</Button>
+        <CardActions sx={{ml:3}}>
+          <Button 
+          // sx={{background: '#052541', color: 'white'}}
+          size="small" 
+          onClick={handleEdit}>EDIT</Button>
         </CardActions>
 
-      </Card> : <Card sx={{ bgcolor: 'text.primary' }}>
+      </Card> : 
+      
+        // <EditProfile/>
+      
+        <Card sx={{ bgcolor: 'text.primary' }}>
           <CardContent color="text.primary">
             <Typography variant="h5" component="div" color="primary.main">
               <Avatar
